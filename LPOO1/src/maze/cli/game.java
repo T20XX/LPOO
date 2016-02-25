d01e70031441f4e69740eae193c5c0304b8971b9
Jorge Vale <jorge-vale1@hotmail.com>
2016-02-25 17:47:11 +0000

    no problem. easy shit

--- LPOO1/src/maze/cli/game.java
+++ LPOO1/src/maze/cli/game.java
@@ -6,7 +6,7 @@ public class game {
 
 	public static void main(String[] args) throws IOException {
 		maze m = new maze();
-		m.read("C:\\Users\\Jorge\\workspace\\LPOO1\\Map");
+		m.read("C:\\Users\\Jorge\\git\\lpoo1\\LPOO1\\Map");
 		m.print();
 	}
 }
